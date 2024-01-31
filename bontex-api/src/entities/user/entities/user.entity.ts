f8d32089b7778e1191e176480ac0552f2cc7774e
KvintSept <smartoctopusz@gmail.com>
2024-01-31 11:46:49 +0200

    first user

--- bontex-api/src/entities/user/entities/user.entity.ts
+++ bontex-api/src/entities/user/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity, ObjectId, ObjectIdColumn } from 'typeorm';
+
+@Entity({ name: 'User' })
+export class UserEntity {
+  @ObjectIdColumn()
+  id: ObjectId;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
--- bontex-api/src/entities/user/user.service.ts
+++ bontex-api/src/entities/user/user.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { MongoRepository } from 'typeorm';
 
 @Injectable()
 export class UserService {
+
+  /**
+   *
+   */
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: MongoRepository<UserEntity>
+  ) {
+    
+  }
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.userRepository.save(createUserDto);
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.userRepository.findOne({where: {id: parseInt(id, 10)}});
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.userRepository.update(id, updateUserDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    return this.userRepository.delete(id);
   }
 }
