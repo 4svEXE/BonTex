1ee02f8d4463d9a02656e439d3403f30e894e1e7
KvintSept <smartoctopusz@gmail.com>
2024-01-31 11:45:52 +0200

    db connection

--- bontex-api/src/app.module.ts
+++ bontex-api/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './entities/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth/auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ConfigModule.forRoot(), TypeOrmModule.forRoot({
+    type:'mongodb',
+    url: process.env.DB_URL,
+    ssl: true,
+    autoLoadEntities: true,
+    synchronize: true
+  }), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- bontex-api/src/main.ts
+++ bontex-api/src/main.ts
@@ -5,4 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+bootstrap().then(() => {
+  console.log("http://localhost:3000/user/");
+  
+});
