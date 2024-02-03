80462155de1d90ac9405ea0ef388fda62960ecd8
KvintSept <smartoctopusz@gmail.com>
2024-02-03 12:35:23 +0200

    signIn with access token

---
+++ bontex-api/src/entities/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(
+    username: string,
+    pass: string,
+  ): Promise<{ access_token: string }> {
+    const user = await this.usersService.findByUsername(username);
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const payload = { sub: user.id, username: user.username };
+    return {
+      access_token: await this.jwtService.signAsync(payload), //generate our JWT from a subset of the user object properties, which we then return as a simple object with a single access_token property
+    };
+  }
+}
