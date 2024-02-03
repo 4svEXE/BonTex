aa2fef3c79e6ceefde704731eb110b45050f4651
KvintSept <smartoctopusz@gmail.com>
2024-02-03 12:34:50 +0200

    getById and getByUsername fixed

--- bontex-api/src/entities/user/user.service.ts
+++ bontex-api/src/entities/user/user.service.ts
@@ -1,22 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { MongoRepository } from 'typeorm';
+import { ObjectId } from 'mongodb';
 
 @Injectable()
 export class UserService {
-
-  /**
-   *
-   */
   constructor(
     @InjectRepository(UserEntity)
-    private userRepository: MongoRepository<UserEntity>
-  ) {
-    
-  }
+    private userRepository: MongoRepository<UserEntity>,
+  ) {}
 
   create(createUserDto: CreateUserDto) {
     return this.userRepository.save(createUserDto);
@@ -26,8 +21,30 @@ export class UserService {
     return this.userRepository.find();
   }
 
-  findOne(id: string) {
-    return this.userRepository.findOne({where: {id: parseInt(id, 10)}});
+  findById(id: string) {
+    try {
+      const objectId = new ObjectId(id);
+      const user = this.userRepository.findOne({ where: { _id: objectId } });
+
+      if (!user) {
+        throw new NotFoundException(`User with id ${id} not found`);
+      }
+
+      return user;
+    } catch (error) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    
+  }
+
+  findByUsername(username: string) {
+    const user = this.userRepository.findOne({ where: { username: username } });
+
+    if (!user) {
+      throw new NotFoundException(`User with username ${username} not found`);
+    }
+
+    return user;
   }
 
   update(id: string, updateUserDto: UpdateUserDto) {
