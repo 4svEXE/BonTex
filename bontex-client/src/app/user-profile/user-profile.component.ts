d39f6e6d0c77fdb79ebc9645ab95fd07076de258
4SVEXE <4sv.exe@gmail.com>
2024-02-09 15:51:53 +0200

    userProufleService

--- bontex-client/src/app/user-profile/user-profile.component.ts
+++ bontex-client/src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,9 @@
-import { AuthenticationService } from 'src/app/services/authentication.service';
 import { Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { UserProfileService } from '../shared/services/user-profile.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -8,23 +11,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent {
-  currentView: string = 'private-dates';
+  currentView: string = '';
+  private viewSubscription!: Subscription;
+
+  navigationItems = [
+    { label: 'Особисті дані', view: 'private-dates' },
+    { label: 'Пароль', view: 'profile-password' },
+    { label: 'Мої замовлення', view: 'profile-orders' },
+    { label: 'Обрані товари', view: 'profile-selected', showCounter: true, counter: 2 },
+    { label: 'Мої відгуки', view: 'profile-myReviews', showCounter: true, counter: 2 },
+    { label: 'Вихід', view: 'profile-logOut' }
+  ];
 
   constructor(
     private authService: AuthenticationService,
-    private router: Router
+    private router: Router,
+    private userProfile: UserProfileService
   ) {}
+
+  ngOnInit(): void {
+    this.viewSubscription = this.userProfile.currentView$.subscribe(
+      (newView) => {
+        this.currentView = newView;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.viewSubscription.unsubscribe();
+  }
 
   logOut() {
     this.authService.logOut();
     this.router.navigate(['']);
   }
 
-  checkIsActive(str: string): boolean {
-    return this.currentView === str;
+  checkIsActive(view: string): boolean {
+    return this.currentView === view;
   }
 
-  setCurrentView(str: string) {
-    this.currentView === str;
+  setCurrentView(view: string) {
+    this.userProfile.updateView(view);
   }
 }
