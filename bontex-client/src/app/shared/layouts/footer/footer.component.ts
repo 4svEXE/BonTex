7bc4ce59b1222b484877548a39c004094b585b95
4SVEXE <4sv.exe@gmail.com>
2024-03-02 19:36:24 +0200

    upgrade: add md and lg sizes

--- bontex-client/src/app/shared/layouts/footer/footer.component.ts
+++ bontex-client/src/app/shared/layouts/footer/footer.component.ts
@@ -1,25 +1,7 @@
 import { Component } from '@angular/core';
 import { SafeHtml } from '@angular/platform-browser';
+import { Sections } from 'src/app/core/variables';
 import { SvgService } from 'src/app/shared/services/svg.service';
-
-interface SectionItem {
-  title: string;
-  items: (MenuItem | ContactItem)[];
-}
-
-interface MenuItem {
-  label: string;
-  link: string;
-  icon?: string;
-  value?: string;
-}
-
-interface ContactItem {
-  label: string;
-  icon: string;
-  value: string;
-  link?: string;
-}
 
 @Component({
   selector: 'app-footer',
@@ -29,46 +11,7 @@ interface ContactItem {
 export class FooterComponent {
   safeSvgCodes: { [key: string]: SafeHtml } = {};
   imgPath: string = 'assets/img/shared/layout/';
-
-  sections: SectionItem[] = [
-    {
-      title: 'Клієнтам',
-      items: [
-        { label: 'Про нас', link: '/' },
-        { label: 'Контакти', link: '/' },
-        { label: 'Угода користувача', link: '/' },
-        { label: 'Питання та відповіді', link: '/' },
-        { label: 'Відгуки про магазин', link: '/' },
-      ],
-    },
-    {
-      title: 'Каталог',
-      items: [
-        { label: 'Всі килими', link: '/' },
-        { label: "Дитячі килими Україна м'які", link: '/' },
-        { label: 'Дитячі килими Україна щільні', link: '/' },
-        { label: 'Дитячі килими Україна круглі', link: '/' },
-        { label: 'Килими бавовняні Туреччина', link: '/' },
-      ],
-    },
-    {
-      title: 'Контакти',
-      items: [
-        { label: '097 328 58 54', icon: 'phone', value: 'tel:+0973285854' },
-        { label: 'м.Івано-Франковськ', icon: 'location', value: '/' },
-        {
-          label: '0973285854',
-          icon: 'whatsApp',
-          value: 'https://wa.me/0973285854',
-        },
-        {
-          label: 'bontex.com.ua@gmail.com',
-          icon: 'email',
-          value: 'mailto:bontex.com.ua@gmail.com',
-        },
-      ],
-    },
-  ];
+  sections = Sections;
 
   constructor(private svgService: SvgService) {}
 
