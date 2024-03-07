e777406a6f4db460f386a05badc37a0139525378
4SVEXE <4sv.exe@gmail.com>
2024-03-07 20:36:18 +0200

    update: styles for recomendet

--- bontex-client/src/app/pages/home/components/recomendet/recomendet.component.ts
+++ bontex-client/src/app/pages/home/components/recomendet/recomendet.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-recomendet',
   templateUrl: './recomendet.component.html',
-  styleUrls: ['./recomendet.component.scss']
+  styleUrls: ['./recomendet.component.scss'],
 })
 export class RecomendetComponent {
+  imgPath: string = 'assets/img/pages/home/';
 
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: [
+      `<img src="${this.imgPath + 'arrow_carousel.svg'}" alt="arrow-left" class="arrow-left"/>`,
+      `<img src="${this.imgPath + 'arrow_carousel.svg'}" alt="arrow-right" />`,
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 4,
+      },
+    },
+    nav: true,
+  };
 }
