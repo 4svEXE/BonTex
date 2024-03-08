88d3612633144cae72b58856b8d93d4376ceef79
4SVEXE <4sv.exe@gmail.com>
2024-03-08 19:04:08 +0200

    styles: some styles

--- bontex-client/src/app/pages/home/components/reviews-home-page/reviews-home-page.component.ts
+++ bontex-client/src/app/pages/home/components/reviews-home-page/reviews-home-page.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-reviews-home-page',
   templateUrl: './reviews-home-page.component.html',
-  styleUrls: ['./reviews-home-page.component.scss']
+  styleUrls: ['./reviews-home-page.component.scss'],
 })
 export class ReviewsHomePageComponent {
+  imgPath: string = 'assets/img/pages/home/';
 
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: [
+      `<img src="${
+        this.imgPath + 'arrow_carousel.svg'
+      }" alt="arrow-left" class="arrow-left"/>`,
+      `<img src="${this.imgPath + 'arrow_carousel.svg'}" alt="arrow-right" />`,
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 4,
+      },
+    },
+    nav: true,
+  };
 }
