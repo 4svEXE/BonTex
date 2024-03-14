a705723bdfd3630226729c0d56fec3c9b52b6709
4SVEXE <4sv.exe@gmail.com>
2024-03-14 19:05:11 +0200

    update: add info and delivery-and-payments to info-routing.module

--- bontex-client/src/app/pages/info/info-routing.module.ts
+++ bontex-client/src/app/pages/info/info-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InfoComponent } from './info.component';
-
+import { DeliveryAndPaymentsComponent } from './components/delivery-and-payments/delivery-and-payments.component';
 
 const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
+
     component: InfoComponent,
+    children: [
+      {
+        path: 'delivery-and-payments',
+        component: DeliveryAndPaymentsComponent,
+      },
+    ],
   },
 ];
 
--- bontex-client/src/app/pages/info/info.module.ts
+++ bontex-client/src/app/pages/info/info.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InfoRoutingModule } from './info-routing.module';
+import { DeliveryAndPaymentsComponent } from './components/delivery-and-payments/delivery-and-payments.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DeliveryAndPaymentsComponent
+  ],
   imports: [CommonModule, InfoRoutingModule],
 })
 export class InfoModule { }
