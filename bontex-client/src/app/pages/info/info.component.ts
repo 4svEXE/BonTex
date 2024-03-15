4a6ee872ad2bbbedecea31025fb9160aa3a390a2
4SVEXE <4sv.exe@gmail.com>
2024-03-15 12:51:57 +0200

    update: add nav-bar links

--- bontex-client/src/app/pages/info/info.component.ts
+++ bontex-client/src/app/pages/info/info.component.ts
@@ -6,15 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./info.component.scss'],
 })
 export class InfoComponent {
-  isActive = false
-
-  navigationItems = [
-    { label: 'Особисті дані', view: 'private-dates' },
-    { label: 'Пароль', view: 'password' },
-    { label: 'Мої замовлення', view: 'orders' },
-    { label: 'Обрані товари', view: 'selected', showCounter: true, counter: 2 },
-    { label: 'Мої відгуки', view: 'reviews', showCounter: true, counter: 2 },
-    { label: 'Вихід', view: 'logout' },
-  ];
-
+  isActive = false;
 }
--- bontex-client/src/app/pages/info/info.module.ts
+++ bontex-client/src/app/pages/info/info.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { InfoRoutingModule } from './info-routing.module';
 
 import { DeliveryAndPaymentsComponent } from './components/delivery-and-payments/delivery-and-payments.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 @NgModule({
