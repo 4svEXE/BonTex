057745be1651af4cb33765674713c2bc7e6ff8e9
4SVEXE <4sv.exe@gmail.com>
2024-03-15 15:57:57 +0200

    add: content for delivery-and0payments

--- bontex-client/src/app/pages/info/components/delivery-and-payments/delivery-and-payments.component.ts
+++ bontex-client/src/app/pages/info/components/delivery-and-payments/delivery-and-payments.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-delivery-and-payments',
   templateUrl: './delivery-and-payments.component.html',
-  styleUrls: ['./delivery-and-payments.component.scss']
+  styleUrls: ['./delivery-and-payments.component.scss', '../../info.component.scss']
 })
 export class DeliveryAndPaymentsComponent {
 
