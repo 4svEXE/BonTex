32ca2d6a126cf88164082fd092bbf21592833ea6
4SVEXE <4sv.exe@gmail.com>
2024-03-15 13:11:20 +0200

    add: new components for info paage

--- bontex-client/src/app/pages/info/info.component.ts
+++ bontex-client/src/app/pages/info/info.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class InfoComponent {
   isActive = false;
+
+  navigationItems = [
+    { label: 'Каталог', route: 'catalog-info' },
+    { label: 'Доставка та оплата', route: 'delivery-and-payments' },
+    { label: 'Обмін та повернення', route: 'exchange-and-returns' },
+    { label: 'Про нас', route: 'about-us' },
+    { label: 'Контакти', route: 'contacts' },
+    { label: 'Угода користувача', route: 'user-agreement' },
+    { label: 'Питання та відповіді', route: 'faq' },
+    { label: 'Відгуки про магазин', route: 'store-reviews' },
+  ];
 }
--- bontex-client/src/app/pages/info/info.module.ts
+++ bontex-client/src/app/pages/info/info.module.ts
@@ -4,11 +4,25 @@ import { InfoRoutingModule } from './info-routing.module';
 
 import { DeliveryAndPaymentsComponent } from './components/delivery-and-payments/delivery-and-payments.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { ExchangeAndReturnsComponent } from './components/exchange-and-returns/exchange-and-returns.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { UserAgreementComponent } from './components/user-agreement/user-agreement.component';
+import { FaqComponent } from './components/faq/faq.component';
+import { StoreReviewsComponent } from './components/store-reviews/store-reviews.component';
+import { CatalogInfoComponent } from './components/catalog-info/catalog-info.component';
 
 
 @NgModule({
   declarations: [
-    DeliveryAndPaymentsComponent
+    DeliveryAndPaymentsComponent,
+    ExchangeAndReturnsComponent,
+    AboutUsComponent,
+    ContactsComponent,
+    UserAgreementComponent,
+    FaqComponent,
+    StoreReviewsComponent,
+    CatalogInfoComponent
   ],
   imports: [ CommonModule, InfoRoutingModule],
 })
