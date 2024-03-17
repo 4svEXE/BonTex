36789ba969b828f9a1fdb7b59e4ce56e16a62194
4SVEXE <4sv.exe@gmail.com>
2024-03-17 15:10:01 +0200

    add: content for faq

--- bontex-client/src/app/pages/info/components/faq/faq.component.ts
+++ bontex-client/src/app/pages/info/components/faq/faq.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.scss']
+  styleUrls: ['./faq.component.scss', '../../info.component.scss']
 })
 export class FaqComponent {
 
