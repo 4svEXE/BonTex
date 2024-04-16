b754ee897b5ba6c766fe932d1aed27463d9ce9e5
4SVEXE <4sv.exe@gmail.com>
2024-04-16 19:10:46 +0300

    update: add responcive swiper to gallery

--- client/src/app/pages/product/components/product-gallery/product-gallery.component.ts
+++ client/src/app/pages/product/components/product-gallery/product-gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-product-gallery',
@@ -20,4 +21,21 @@ export class ProductGalleryComponent implements OnChanges {
   changePhoto(photo: string) {
     this.currentPhoto = photo;
   }
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: true,
+    autoplay: false,
+    navSpeed: 700,
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+    nav: false,
+    autoHeight: true
+  };
 }
