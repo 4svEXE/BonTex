b156bcb5d94ec8f0857c795ff550ed78cb42a793
4SVEXE <4sv.exe@gmail.com>
2024-04-17 12:29:36 +0300

    update: product-nfo - add header nav

--- client/src/app/pages/product/components/product-info/product-info.component.ts
+++ client/src/app/pages/product/components/product-info/product-info.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from 'src/app/core/interfaces';
 
 @Component({
   selector: 'app-product-info',
   templateUrl: './product-info.component.html',
-  styleUrls: ['./product-info.component.scss']
+  styleUrls: ['./product-info.component.scss'],
 })
 export class ProductInfoComponent {
+  @Input() product!: Product;
 
+  headerNavs = [
+    { label: 'Опис', link: 'description' },
+    { label: 'Оплата', link: 'payment' },
+    { label: 'Повернення та обмін', link: 'returns-exchanges' },
+    { label: 'Доставка', link: 'delivery' },
+    { label: 'Відгуки', link: 'reviews' }
+  ];
+  currentNav = 'description';
+
+  reviews = 0
+
+  setCurrentNav(nav: string) {
+    this.currentNav = nav;
+  }
 }
