4d73f67377c52e8b47a95ce1e5837d32e7315f8b
4SVEXE <4sv.exe@gmail.com>
2024-04-20 23:47:55 +0300

    add: (custom-notification-modal):  add component and stylefix product-add-review and loader

---
+++ client/src/app/shared/layout/notification-modal/notification-modal.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input } from '@angular/core';
+import { NgxSmartModalService } from 'ngx-smart-modal';
+
+import { SafeSvg } from 'src/app/core/interfaces';
+import { SvgService } from 'src/app/core/services/svg.service';
+
+@Component({
+  selector: 'app-notification-modal',
+  templateUrl: './notification-modal.component.html',
+  styleUrls: ['./notification-modal.component.scss'],
+})
+export class NotificationModalComponent {
+  @Input() title!: string;
+  @Input() text!: string;
+  @Input() timer = 2000;
+
+  isPausedAutoclose = false;
+
+  safeSvgCodes: SafeSvg = this.svgService.getSafeSvgCodes();
+
+  constructor(
+    public ngxSmartModalService: NgxSmartModalService,
+    private svgService: SvgService
+  ) {}
+
+  onOpenModal() {
+    this.setModalAutocloseTimeout();
+  }
+
+  timeout = setTimeout(() => {}, 0);
+  setModalAutocloseTimeout() {
+    clearTimeout(this.timeout);
+
+    this.timeout = setTimeout(() => {
+      if (!this.isPausedAutoclose) {
+        this.ngxSmartModalService.close('notificationModal');
+      }
+    }, this.timer);
+  }
+
+  onMouseEnter() {
+    this.isPausedAutoclose = true;
+  }
+
+  onMouseLeave() {
+    this.isPausedAutoclose = false;
+    this.setModalAutocloseTimeout();
+  }
+}
